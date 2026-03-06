1a0928219e8c6b661d7adac66f6f1b5cb69b458e
Christian Nygard <seajay.nygard@gmail.com>
2026-03-06 14:10:00 -0500

    something

--- setup_sample.sh
+++ setup_sample.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh fp@raspberrypi.local "source ~/fp/venv/bin/activate; python ~/fp/setup_sample.py"
+ssh -L 5000:localhost:5000 fp@fp.local "source ~/fp/venv/bin/activate; python ~/fp/setup_sample.py"
